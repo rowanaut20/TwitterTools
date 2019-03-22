40b65579eab47130260a3d8d0e40c573c2635c1f
Autumn <autumnummie@gmail.com>
2019-03-22 12:46:40 -0500

    Create globals.php

---
+++ globals.php
@@ -0,0 +1,27 @@
+<?php
+
+class Globals {
+
+function username() {
+  return "jack";
+}
+
+function getConsumerKey() {
+  return "";
+}
+
+function getConsumerSecret() {
+  return "";
+}
+
+function getAccessToken() {
+  return "";
+}
+
+function getAccessTokenSecret() {
+  return "";
+}
+
+}
+
+?>
